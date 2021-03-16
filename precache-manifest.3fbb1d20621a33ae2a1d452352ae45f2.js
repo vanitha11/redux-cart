77cf1f67b6fa60437dd8b66034cb8c6fdde894b7
vanitha11 <vanivenkat1@gmail.com>
2021-03-16 08:08:34 +0530

    Updates

---
+++ precache-manifest.3fbb1d20621a33ae2a1d452352ae45f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3234095f55e8ac3c273b4acdce9cf2",
+    "url": "/redux-cart/index.html"
+  },
+  {
+    "revision": "5e7d02850477fde7aebd",
+    "url": "/redux-cart/static/css/main.c463a086.chunk.css"
+  },
+  {
+    "revision": "dda2b3ee9fac5a6891ff",
+    "url": "/redux-cart/static/js/2.4bb05192.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/redux-cart/static/js/2.4bb05192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7d02850477fde7aebd",
+    "url": "/redux-cart/static/js/main.456e433f.chunk.js"
+  },
+  {
+    "revision": "b3f37f5e286c33cf80d9",
+    "url": "/redux-cart/static/js/runtime-main.b036b403.js"
+  }
+]);
